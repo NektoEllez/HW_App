c68e9b8098ebbe5e77b24d637ea584ceda0a96e8
Ivan Potyng <nekto_ellez@icloud.com>
2023-07-14 19:58:59 +0530

    Initial Commit

---
+++ HW_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW_App
+//
+//  Created by Иван Марин on 14.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
